e4b152f1cdc4cb68da5024aaf663e4044aff65a5
Madhavi Itikala <madhavi.i@abhibus.com>
2024-08-27 00:20:27 +0530

    small chnage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './homePage.module.css';
-import BackgroundVideo from '../../assets/videoplayback.mp4'; // Path to your video file
+import BackgroundVideo from '../../assets/videoplayback.webm'; // Path to your video file
 
 const HomePage = () => {
   return (
     <div className={styles.homePage}>
       <video className={styles.backgroundVideo} autoPlay muted loop>
-        <source src={BackgroundVideo} type="video/mp4" />
+        <source src={BackgroundVideo} type="video/mp4/webm" />
         Your browser does not support the video tag.
       </video>
       <div className={styles.overlay}></div>
