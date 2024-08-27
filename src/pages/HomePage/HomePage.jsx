87423ccf8a91cdc378beb7e9b5e3e04eb88e4185
Madhavi Itikala <madhavi.i@abhibus.com>
2024-08-27 21:56:03 +0530

    chnaged to gif

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import styles from './homePage.module.css';
-import BackgroundVideo from '../../assets/videoplayback.webm'; // Path to your video file
+import BackgroundGif from '../../assets/videoplayback.gif'; // Path to your GIF file
 
 const HomePage = () => {
   return (
     <div className={styles.homePage}>
-      <video className={styles.backgroundVideo} autoPlay muted loop>
-        <source src={BackgroundVideo} type="video/webm" /> {/* Corrected type */}
-        Your browser does not support the video tag.
-      </video>
+      <img className={styles.backgroundGif} src={BackgroundGif} alt="Background" /> {/* Updated to use GIF */}
       <div className={styles.overlay}></div>
       <div className={styles.content}>
         <h1 className={styles.fadeText}>VIBHUAYA TECHNOLOGIES</h1>
